0af8d1a590bef26128029d1eb80009486c962667
AlexsanderDiego <alexdiegoat@gmail.com>
2023-05-06 23:18:17 -0300

    Corrigindo tabela IMC

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ function imc() {
 
         if (valorIMC < 18.5) {
             classificacao = 'abaixo do peso!'
-        }else if (valorIMC < 25) {
+        }else if (valorIMC < 24.9) {
             classificacao = 'com peso ideal. Parabéns!'
-        }else if (valorIMC < 30) {
+        }else if (valorIMC < 29.9) {
             classificacao = 'levemente acima do peso!'
-        }else if (valorIMC < 35) {
-            classificacao = 'obeso grau I!'
-        }else if (valorIMC < 40) {
-            classificacao = 'obeso grau II!'
+        }else if (valorIMC < 34.9) {
+            classificacao = 'com obesidade grau I!'
+        }else if (valorIMC < 39.9) {
+            classificacao = 'com obesidade grau II!'
         }else {
-            classificacao = 'com obesidade grau III. Cuidado'
+            classificacao = 'com obesidade grau III. Cuidado!'
         }
         resultado.textContent = `${nome} seu IMC é ${valorIMC} e voce está ${classificacao}`
     } 
